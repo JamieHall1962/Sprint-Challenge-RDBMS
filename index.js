246f4f9af5da5d76e58efbdbc58d7fd9e2322ecc
Jamie Hall <hall.jamie@gmail.com>
2019-02-22 11:30:22 -0500

    Initial commit. Structure in place

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const projectsRoutes = require("./projects/router");
+const actionsRoutes = require("./actions/router");
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+server.use("/api/projects", projectsRoutes);
+server.use("/api/actions", actionsRoutes);
+
+const port = 4000;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
